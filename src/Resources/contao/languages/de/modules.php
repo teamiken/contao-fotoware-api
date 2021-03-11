889fa6c005a0635a1c0a0fd939f99c70db1ed540
sophia-ve <sve@teamiken.de>
2021-03-11 11:17:59 +0100

    change frontend modul

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,4 +1,4 @@
 <?php
 
 // Frontend modules
-$GLOBALS['TL_LANG']['FMD']['fotowareApi'] = ['Fotoware Api', 'Gibt den Text "Fotoware Api" aus'];
+$GLOBALS['TL_LANG']['FMD']['fotowareApi'] = ['Fotoware Api!', 'Gibt den Text "Fotoware Api" aus'];
