6182645f088377eccc19bae4c1b813cc16d9f541
sophia-ve <sve@teamiken.de>
2021-03-11 10:16:29 +0100

    config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,4 @@
 <?php
 
 // Frontend modules
-$GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'teamiken\ContaoFotowareApiBundle\Module\FotowareApiModule';
+$GLOBALS['FE_MOD']['miscellaneous']['fotowareApi'] = 'teamiken\ContaoFotowareApiBundle\Module\FotowareApiModule';
